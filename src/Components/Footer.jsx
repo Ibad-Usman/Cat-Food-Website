2ebe5ad82681e6ac09a23ec7c334485bbbb0a551
unknown <ibadusman.22@gmail.com>
2025-11-25 20:02:50 +0500

    your message

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -38,10 +38,10 @@ const Footer = ({color}) => {
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
               <ul className="text-gray-600 dark:text-gray-400">
                 <li className="mb-4">
-                  <a href="#" className="hover:underline">Privacy Policy</a>
+                  <a href="javascript:void(0)" className="hover:underline">Privacy Policy</a>
                 </li>
                 <li>
-                  <a href="#" className="hover:underline">Terms & Conditions</a>
+                  <a href="javascript:void(0)" className="hover:underline">Terms & Conditions</a>
                 </li>
               </ul>
             </div>
